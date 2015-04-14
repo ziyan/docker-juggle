c1d1a0907268f6689e53cb58110d6577b7b31ec2
ziyan <zhou@ziyan.info>
2015-04-14 23:36:37 +0900

    Compression done.

--- cmd/docker-juggle-compress/main.go
+++ cmd/docker-juggle-compress/main.go
@@ -8,6 +8,12 @@ import (
 	"io"
 	"os"
 	"runtime"
+	"strings"
+	"crypto/sha1"
+	"encoding/hex"
+	"bytes"
+	"strconv"
+	"encoding/base64"
 )
 
 var (
@@ -22,42 +28,112 @@ func main() {
 	flag.Parse()
 
 	if err := run(); err != nil {
-		glog.Errorf("docker-juggle-compress: %v", err)
-		os.Exit(1)
+		glog.Exitf("docker-juggle-compress: %v", err)
 	}
 }
 
 func run() error {
 
-	if _, err := docker.History(*base); err != nil {
-		return err
-	}
-
-	saved, cmd, err := docker.Save(*base)
+	history, index, err := prepare(*base)
 	if err != nil {
 		return err
 	}
-	defer func() {
-		if err := cmd.Wait(); err != nil {
-			panic(err)
-		}
-	}()
-
-	for {
-		hdr, err := saved.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-
-		glog.Warningf("docker-juggle-compress: %s", hdr.Name)
-	}
 
 	input := tar.NewReader(os.Stdin)
+	output := tar.NewWriter(os.Stdout)
+	defer output.Close()
+
+	if err := compress(output, input, history, index); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func prepare(base string) ([]string, map[string][]string, error) {
+
+	// get history of base image
+	history, err := docker.History(base)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// save the base image as a tar
+	saved, cmd, err := docker.Save(history[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	defer cmd.Wait()
+
+	// index base image
+	index, err := index(saved)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// cleanup
+	if err := cmd.Wait(); err != nil {
+		return nil, nil, err
+	}
+
+	return history, index, nil
+}
+
+func index(saved *tar.Reader) (map[string][]string, error) {
+
+	index := make(map[string][]string)
+
 	for {
-		hdr, err := input.Next()
+		header, err := saved.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		if !strings.HasSuffix(header.Name, "/layer.tar") {
+			continue
+		}
+
+		layer := strings.TrimSuffix(header.Name, "/layer.tar")
+		glog.V(2).Infof("index: layer: %s", layer)
+
+		reader := tar.NewReader(saved)
+
+		for {
+			header, err := reader.Next()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return nil, err
+			}
+
+			if header.Size == 0 {
+				continue
+			} 
+
+			// calculate hash
+			hasher := sha1.New()
+			if _, err := io.Copy(hasher, reader); err != nil {
+				return nil, err
+			}
+			hash := hex.EncodeToString(hasher.Sum(nil))
+
+			glog.V(2).Infof("index: layer: %s: %s = %s", layer, header.Name, hash)
+			index[hash] = []string{layer, header.Name}
+		}
+	}
+
+	glog.V(2).Infof("index: %d files", len(index))
+	return index, nil
+}
+
+func compress(output *tar.Writer, input *tar.Reader, history []string, index map[string][]string) error {
+
+	for {
+		header, err := input.Next()
 		if err == io.EOF {
 			break
 		}
@@ -65,7 +141,124 @@ func run() error {
 			return err
 		}
 
-		glog.Warningf("docker-juggle-compress: %s", hdr.Name)
+		// straight copy metadata or small layer
+		if !strings.HasSuffix(header.Name, "/layer.tar") {
+			if err := output.WriteHeader(header); err != nil {
+				return err
+			}
+			if _, err := io.Copy(output, input); err != nil {
+				return err
+			}
+			if err := output.Flush(); err != nil {
+				return err
+			}
+			continue
+		}
+
+		layer := strings.TrimSuffix(header.Name, "/layer.tar")
+		found := false
+		for _, l := range history {
+			if l == layer {
+				found = true
+				break
+			}
+		}
+		if found {
+			glog.V(2).Infof("compress: %s: no changes", layer)
+			continue
+		}
+
+		reader := tar.NewReader(input)
+		buffer := bytes.NewBuffer(nil)
+		writer := tar.NewWriter(buffer)
+		defer writer.Close()
+		if err := diff(writer, reader, index); err != nil {
+			return err
+		}
+		if err := writer.Close(); err != nil {
+			return err
+		}
+
+		header.Name = layer + "/diff.tar"
+		header.Size = int64(buffer.Len())
+		if err := output.WriteHeader(header); err != nil {
+			return err
+		}
+		if _, err := io.Copy(output, buffer); err != nil {
+			return err
+		}
+		if err := output.Flush(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func diff(writer *tar.Writer, reader *tar.Reader, index map[string][]string) error {
+
+	for {
+		header, err := reader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		// straight copy small files
+		if header.Size <= 0 {
+			if err := writer.WriteHeader(header); err != nil {
+				return err
+			}
+			if _, err := io.Copy(writer, reader); err != nil {
+				return err
+			}
+			if err := writer.Flush(); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// hash file and make a copy
+		buffer := bytes.NewBuffer(nil)
+		hasher := sha1.New()
+		if _, err := io.Copy(io.MultiWriter(hasher, buffer), reader); err != nil {
+			return err
+		}
+		hash := hex.EncodeToString(hasher.Sum(nil))
+
+		// copy new file
+		info, ok := index[hash]
+		if !ok {
+			if header.Size != int64(buffer.Len()) {
+				panic("buffer length differs from size in header")
+			}
+			if err := writer.WriteHeader(header); err != nil {
+				return err
+			}
+			if _, err := io.Copy(writer, buffer); err != nil {
+				return err
+			}
+			if err := writer.Flush(); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// use index info
+		header.Xattrs = make(map[string]string, 3)
+		header.Xattrs["docker.juggle.layer"] = info[0]
+		header.Xattrs["docker.juggle.name"] = base64.StdEncoding.EncodeToString([]byte(info[1]))
+		header.Xattrs["docker.juggle.size"] = strconv.FormatInt(header.Size, 10)
+		header.Size = 0
+
+		if err := writer.WriteHeader(header); err != nil {
+			return err
+		}
+		if err := writer.Flush(); err != nil {
+			return err
+		}
 	}
 
 	return nil
--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -4,16 +4,37 @@ import (
 	"archive/tar"
 	"github.com/golang/glog"
 	"os/exec"
+	"bufio"
 )
 
 func History(image string) ([]string, error) {
-	output, err := exec.Command("docker", "history", "-q", "--no-trunc", image).Output()
+	history := make([]string, 0, 10)
+
+	cmd := exec.Command("docker", "history", "-q", "--no-trunc", image)
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		return nil, err
 	}
 
-	glog.Warningf("docker-juggle-compress: %s", output)
-	return nil, nil
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	defer cmd.Wait()
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		history = append(history, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return nil, err
+	}
+
+	glog.V(2).Infof("docker: history: %v", history)
+	return history, nil
 }
 
 func Save(image string) (*tar.Reader, *exec.Cmd, error) {
